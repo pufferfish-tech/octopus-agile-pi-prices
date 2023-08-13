6e9fc1ba65f687541e904e814d22be8b237c143f
Nick Brook <nrbrook@gmail.com>
2023-08-13 12:20:36 +0100

    Improvements to code and fixed bar colour to match current price colour

--- octoprice_main_inky.py
+++ octoprice_main_inky.py
@@ -17,6 +17,9 @@ import datetime
 import pytz
 import time
 from urllib.request import pathname2url
+import get_prices_from_db
+
+low_price = 14.8
 
 ##  -- Detect display type automatically
 try:
@@ -24,380 +27,128 @@ try:
 except TypeError:
     raise TypeError("You need to update the Inky library to >= v1.1.0")
 
-try:
-    # connect to the database in rw mode so we can catch the error if it doesn't exist
-    DB_URI = 'file:{}?mode=rw'.format(pathname2url('agileprices.sqlite'))
-    conn = sqlite3.connect(DB_URI, uri=True)
-    cur = conn.cursor()
-    print('Connected to database...')
-except sqlite3.OperationalError as error:
-    # handle missing database case
-    raise SystemExit('Database not found - you need to run store_prices.py first.') from error
-
 inky_display.set_border(inky_display.WHITE)
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 
-# find current time and convert to year month day etc
-the_now = datetime.datetime.now(datetime.timezone.utc)
-the_now_local = the_now.astimezone(pytz.timezone('Europe/London'))
+# array of up to 48 dicts, sorted by time
+prices = get_prices_from_db(48)
 
-the_year = the_now.year
-the_month = the_now.month
-the_hour = the_now.hour
-the_day = the_now.day
-if the_now.minute < 30:
-	the_segment = 0
-else:
-	the_segment = 1
+if len(prices) == 0:
+	print('No prices found!')
+	exit(1)
 
-print ('segment:')
-print (the_segment)
+min_price = {'price': 999}
+min_price_index = 0
+total_price = 0
 
-# select from db where record == the above
-cur.execute("SELECT * FROM prices WHERE year=? AND month=? AND day=? AND hour=? AND segment=?",
-		(the_year, the_month, the_day, the_hour, the_segment))
+for idx, price in enumerate(prices):
+	if price['price'] < min_price['price']:
+		min_price = price
+		min_price_index = idx
 
-rows = cur.fetchall()
+	total_price += price['price']
 
-for row in rows:
-	print(row[5])
+avg_price = total_price / len(prices)
+time_to_min_price = min_price_index / 2
 
-# get price
-current_price = row[5] # literally this is hardcoded tuple. DONT ADD ANY EXTRA FIELDS TO THAT TABLE on the sqlite db or you'll get something that isn't price.
-
-# Find Next Price
-# find current time and convert to year month day etc
-the_now = datetime.datetime.now(datetime.timezone.utc)
-now_plus_10 = the_now + datetime.timedelta(minutes = 30)
-the_year = now_plus_10.year
-the_month = now_plus_10.month
-the_hour = now_plus_10.hour
-the_day = now_plus_10.day
-if now_plus_10.minute < 30:
-	the_segment = 0
-else:
-	the_segment = 1
-
-print ('segment+1:')
-print (the_segment)
-
-# select from db where record == the above
-cur.execute("SELECT * FROM prices WHERE year=? AND month=? AND day=? AND hour=? AND segment=?",
-		(the_year, the_month, the_day, the_hour, the_segment))
-
-rows = cur.fetchall()
-
-for row in rows:
-	print(row[5])
-
-# get price
-next_price = row[5] # literally this is peak tuple. DONT ADD ANY EXTRA FIELDS TO THAT TABLE
+min_price_str = "{0:.1f}p".format(min_price['price'])
+min_price_duration_str = "{0:.1f}hrs".format(min_price_index/2)
+min_price_time = min_price['valid_from'][11:16]
+print("Lowest price {0} in {1} at {2}".format(min_price_str, min_price_duration_str, min_price_time))
 
 
-
-# Find Next+1 Price
-# find current time and convert to year month day etc
-the_now = datetime.datetime.now(datetime.timezone.utc)
-now_plus_10 = the_now + datetime.timedelta(minutes = 60)
-the_year = now_plus_10.year
-the_month = now_plus_10.month
-the_hour = now_plus_10.hour
-the_day = now_plus_10.day
-if now_plus_10.minute < 30:
-	the_segment = 0
-else:
-	the_segment = 1
-
-print ('segment:')
-print (the_segment)
-
-# select from db where record = ^
-cur.execute("SELECT * FROM prices WHERE year=? AND month=? AND day=? AND hour=? AND segment=?",
-		(the_year, the_month, the_day, the_hour, the_segment))
-
-rows = cur.fetchall()
-
-for row in rows:
-	print(row[5])
+def colour_for_price(price):
+	return inky_display.RED if price > low_price else inky_display.BLACK
 
 
+def draw_next_price(index, size, x_offset):
+	if(len(prices) < index + 1):
+		return
 
-# get price
-nextp1_price = row[5] # literally this is peak tuple. DONT ADD ANY EXTRA FIELDS TO THAT TABLE
-
-
-
-# Find Next+2 Price
-# find current time and convert to year month day etc
-the_now = datetime.datetime.now(datetime.timezone.utc)
-now_plus_10 = the_now + datetime.timedelta(minutes = 90)
-the_year = now_plus_10.year
-the_month = now_plus_10.month
-the_hour = now_plus_10.hour
-the_day = now_plus_10.day
-if now_plus_10.minute < 30:
-	the_segment = 0
-else:
-	the_segment = 1
-
-print ('segment:')
-print (the_segment)
-
-# select from db where record == the above
-cur.execute("SELECT * FROM prices WHERE year=? AND month=? AND day=? AND hour=? AND segment=?",
-		(the_year, the_month, the_day, the_hour, the_segment))
-rows = cur.fetchall()
-for row in rows:
-	print(row[5])
-# get price
-nextp2_price = row[5] # literally this is peak tuple. DONT ADD ANY EXTRA FIELDS TO THAT TABLE
+	# NEXT
+	message = "{0}:{1:.1f}p".format(index + 2, prices[index+1]['price'])
+	font = ImageFont.truetype(FredokaOne, size)
+	w2, h2 = font.getsize(message)
+	x = x_offset
+	y = size * index
+	colour = colour_for_price(prices[index + 1]['price'])
+	draw.text((x,y), message, colour, font)
 
 
+def draw_graph(bar_width, bar_height_per_p, bar_base_offset):
+	if(min_price['price'] < 0):
+		bar_base_offset += min_price['price'] * bar_height_per_p
 
-# attempt to make an list of the next 42 hours of values
-prices = []
-for offset in range(0, 48):  ##24h = 48 segments
-	min_offset = 30 * offset
-	the_now = datetime.datetime.now(datetime.timezone.utc)
-	now_plus_offset = the_now + datetime.timedelta(minutes=min_offset)
-	the_year = now_plus_offset.year
-	the_month = now_plus_offset.month
-	the_hour = now_plus_offset.hour
-	the_day = now_plus_offset.day
-	if now_plus_offset.minute < 30:
-		the_segment = 0
-	else:
-		the_segment = 1
-	cur.execute("SELECT * FROM prices WHERE year=? AND month=? AND day=? AND hour=? AND segment=?",
-				(the_year, the_month, the_day, the_hour, the_segment))
-	# rows = cur.fetchall()
-	# get price
-	row = cur.fetchone()
-	if row is None:
-		prices.append(999) # we don't have that price yet!
-	else:
-		prices.append(row[5])
+	for i, price in enumerate(prices):
+		scaled_price = price['price'] * bar_height_per_p
+		ink_color = colour_for_price(price['price'])
+		# takes a bit of thought this next bit, draw a rectangle from say x =  2i to 2(i-1) for each plot value
+		# pixels_per_w defines the horizontal scaling factor (2 seems to work)
+		draw.rectangle((bar_width*i,bar_base_offset,bar_width*(i-1),(bar_base_offset-scaled_price)),ink_color)
+
+
+def draw_all(current_font_size, current_y_offset, right_column_offset, right_column_text_size, bar_width, bar_height_per_p, bar_base_offset, lowest_price_font_size, lowest_price_y_offset):
+	# Current price
+	font = ImageFont.truetype(FredokaOne, current_font_size)
+	message = "{0:.1f}".format(prices[0]['price']) + "p"
+	w, h = font.getsize(message)
+	#x = (inky_display.WIDTH / 2) - (w / 2)
+	#y = (inky_display.HEIGHT / 2) - (h / 2)
+	x = 0
+	y = current_y_offset
+	draw.text((x, y), message, colour_for_price(prices[0]['price']), font)
+
+
+	# Next prices
+	draw_next_price(0, right_column_text_size, right_column_offset)
+	draw_next_price(1, right_column_text_size, right_column_offset)
+	draw_next_price(2, right_column_text_size, right_column_offset)
+
+
+	# Graph
+	draw_graph(bar_width, bar_height_per_p, bar_base_offset)
+
+
+	# Lowest price
+	# draw the bottom right min price and how many hours that is away
+	font = ImageFont.truetype(FredokaOne, lowest_price_font_size)
+	msg = "min:"+min_price_str
+	draw.text((right_column_offset,lowest_price_y_offset), msg, inky_display.BLACK, font)
+	# we know how many half hours to min price, now figure it out in hours.
+	msg = "in:"+min_price_duration_str
+	draw.text((right_column_offset,lowest_price_y_offset + lowest_price_font_size), msg, inky_display.BLACK, font)
+	# get time of cheapest
+	msg = "at " + min_price_time
+	draw.text((right_column_offset,lowest_price_y_offset + lowest_price_font_size * 2), msg, inky_display.BLACK, font)
+	
 
 if (inky_display.WIDTH == 212): #low res display
+	draw_all(
+		current_font_size=60,
+		current_y_offset=-5,
+		right_column_offset=145,
+		right_column_text_size=20,
+		bar_width=3,
+		bar_height_per_p=2,
+		bar_base_offset=104,
+		lowest_price_font_size=15,
+		lowest_price_y_offset=60
+		)
 
-	font = ImageFont.truetype(FredokaOne, 60)
-	message = "{0:.1f}".format(current_price) + "p"
-	w, h = font.getsize(message)
-	#x = (inky_display.WIDTH / 2) - (w / 2)
-	#y = (inky_display.HEIGHT / 2) - (h / 2)
-	x = 0
-	y = -5
-
-	if (current_price > 14.8):
-		draw.text((x, y), message, inky_display.RED, font)
-	else:
-		draw.text((x, y), message, inky_display.BLACK, font)
-
-	right_column = 145
-
-	# NEXT
-	message = "2:" + "{0:.1f}".format(next_price) + "p"
-	font = ImageFont.truetype(FredokaOne, 20)
-	w2, h2 = font.getsize(message)
-	x = right_column
-	y = 0
-	if (next_price > 14.8):
-		draw.text((x,y), message, inky_display.RED, font)
-	else:
-		draw.text((x, y), message, inky_display.BLACK, font)
-
-	# NEXT
-	message = "3:" + "{0:.1f}".format(nextp1_price) + "p"
-	font = ImageFont.truetype(FredokaOne, 20)
-	w3, h3 = font.getsize(message)
-	x = right_column
-	y = 20
-
-	if (nextp1_price > 14.8):
-		draw.text((x,y), message, inky_display.RED, font)
-	else:
-		draw.text((x, y), message, inky_display.BLACK, font)
-
-	# NEXT
-	message = "4:" + "{0:.1f}".format(nextp2_price) + "p"
-	font = ImageFont.truetype(FredokaOne, 20)
-	w3, h3 = font.getsize(message)
-	x = right_column
-	y = 40
-
-	if (nextp2_price > 14.8):
-		draw.text((x,y), message, inky_display.RED, font)
-	else:
-		draw.text((x, y), message, inky_display.BLACK, font)
-
-	pixels_per_h = 2  # how many pixels 1p is worth
-	pixels_per_w = 3  # how many pixels 1/2 hour is worth
-	chart_base_loc = 104  # location of the bottom of the chart on screen in pixels
-	#chart_base_loc = 85  # location of the bottom of the chart on screen in pixels
-	number_of_vals_to_display = 48 # 36 half hours = 18 hours
-
-	# plot the graph
-	#lowest_price_next_24h = min(i for i in prices if i > 0)
-	lowest_price_next_24h = min(i for i in prices)
-	if (lowest_price_next_24h < 0):
-		chart_base_loc = 104 + lowest_price_next_24h*pixels_per_h - 2 # if we have any negative prices, shift the base of the graph up! 
-
-	print("lowest price Position:", prices.index(lowest_price_next_24h))
-	print("low Value:", lowest_price_next_24h)
-
-	# go through each hour and get the value
-
-	for i in range(0,number_of_vals_to_display):
-		if prices[i] < 999:
-			scaled_price = prices[i] * pixels_per_h # we're scaling it by the value above
-
-			if prices[i] <= (lowest_price_next_24h + 1):   # if within 1p of the lowest price, display in black
-				ink_color = inky_display.BLACK
-			else:
-				ink_color = inky_display.RED
-
-			# takes a bit of thought this next bit, draw a rectangle from say x =  2i to 2(i-1) for each plot value
-			# pixels_per_w defines the horizontal scaling factor (2 seems to work)
-			draw.rectangle((pixels_per_w*i,chart_base_loc,((pixels_per_w*i)-pixels_per_w),(chart_base_loc-scaled_price)),ink_color)
-
-	#draw minimum value on chart  <- this doesn't seem to work yet
-	# font = ImageFont.truetype(FredokaOne, 15)
-	# msg = "{0:.1f}".format(lowest_price_next_24h) + "p"
-	# draw.text((4*(minterval-1),110),msg, inky_display.BLACK, font)
-
-	# draw the bottom right min price and how many hours that is away
-	font = ImageFont.truetype(FredokaOne, 15)
-	msg = "min:"+"{0:.1f}".format(lowest_price_next_24h) + "p"
-	draw.text((right_column,60), msg, inky_display.BLACK, font)
-	# we know how many half hours to min price, now figure it out in hours.
-	minterval = (round(prices.index(lowest_price_next_24h)/2))
-	print ("minterval:"+str(minterval))
-	msg = "in:"+str(minterval)+"hrs"
-	draw.text((right_column,75), msg, inky_display.BLACK, font)
-
-	# and convert that to an actual time
-	# note that this next time will not give you an exact half hour if you don't run this at an exact half hour eg cron
-	# because it's literally just adding n * 30 mins!
-	# could in future add some code to round to 30 mins increments but it works for now.
-
-	min_offset = prices.index(lowest_price_next_24h) * 30
-	time_of_cheapest = the_now_local + datetime.timedelta(minutes=min_offset)
-	print("cheapest at " + str(time_of_cheapest))
-	print("which is: "+ str(time_of_cheapest.time())[0:5])
-	time_of_cheapest_formatted = "at " + (str(time_of_cheapest.time())[0:5])
-	font = ImageFont.truetype(FredokaOne, 15)
-	draw.text((right_column,90), time_of_cheapest_formatted, inky_display.BLACK, font)
 
 else: #high res display
-
-	font = ImageFont.truetype(FredokaOne, 72)
-	message = "{0:.1f}".format(current_price) + "p"
-	w, h = font.getsize(message)
-	#x = (inky_display.WIDTH / 2) - (w / 2)
-	#y = (inky_display.HEIGHT / 2) - (h / 2)
-	x = 0
-	y = -10
-
-	if (current_price > 14.8):
-		draw.text((x, y), message, inky_display.RED, font)
-	else:
-		draw.text((x, y), message, inky_display.BLACK, font)
-
-	right_column = 172
-
-	# NEXT
-	message = "2:" + "{0:.1f}".format(next_price) + "p"
-	font = ImageFont.truetype(FredokaOne, 23)
-	w2, h2 = font.getsize(message)
-	x = right_column
-	y = 0
-	if (next_price > 14.8):
-		draw.text((x,y), message, inky_display.RED, font)
-	else:
-		draw.text((x, y), message, inky_display.BLACK, font)
-
-	# NEXT
-	message = "3:" + "{0:.1f}".format(nextp1_price) + "p"
-	font = ImageFont.truetype(FredokaOne, 23)
-	w3, h3 = font.getsize(message)
-	x = right_column
-	y = 23
-
-	if (nextp1_price > 14.8):
-		draw.text((x,y), message, inky_display.RED, font)
-	else:
-		draw.text((x, y), message, inky_display.BLACK, font)
-
-	# NEXT
-	message = "4:" + "{0:.1f}".format(nextp2_price) + "p"
-	font = ImageFont.truetype(FredokaOne, 23)
-	w3, h3 = font.getsize(message)
-	x = right_column
-	y = 46
-
-	if (nextp2_price > 14.8):
-		draw.text((x,y), message, inky_display.RED, font)
-	else:
-		draw.text((x, y), message, inky_display.BLACK, font)
-
-	pixels_per_h = 2.3  # how many pixels 1p is worth
-	pixels_per_w = 3.5  # how many pixels 1/2 hour is worth
-	chart_base_loc = 121  # location of the bottom of the chart on screen in pixels
-	#chart_base_loc = 85  # location of the bottom of the chart on screen in pixels
-	number_of_vals_to_display = 48 # 36 half hours = 18 hours
-
-	# plot the graph
-	#lowest_price_next_24h = min(i for i in prices if i > 0)
-	lowest_price_next_24h = min(i for i in prices)
-	if (lowest_price_next_24h < 0):
-		chart_base_loc = 104 + lowest_price_next_24h*pixels_per_h - 2 # if we have any negative prices, shift the base of the graph up! 
-
-	print("lowest price Position:", prices.index(lowest_price_next_24h))
-	print("low Value:", lowest_price_next_24h)
-
-	# go through each hour and get the value
-
-	for i in range(0,number_of_vals_to_display):
-		if prices[i] < 999:
-			scaled_price = prices[i] * pixels_per_h # we're scaling it by the value above
-
-			if prices[i] <= (lowest_price_next_24h + 1):   # if within 1p of the lowest price, display in black
-				ink_color = inky_display.BLACK
-			else:
-				ink_color = inky_display.RED
-
-			# takes a bit of thought this next bit, draw a rectangle from say x =  2i to 2(i-1) for each plot value
-			# pixels_per_w defines the horizontal scaling factor (2 seems to work)
-			draw.rectangle((pixels_per_w*i,chart_base_loc,((pixels_per_w*i)-pixels_per_w),(chart_base_loc-scaled_price)),ink_color)
-
-	#draw minimum value on chart  <- this doesn't seem to work yet
-	# font = ImageFont.truetype(FredokaOne, 15)
-	# msg = "{0:.1f}".format(lowest_price_next_24h) + "p"
-	# draw.text((4*(minterval-1),110),msg, inky_display.BLACK, font)
-
-	# draw the bottom right min price and how many hours that is away
-	font = ImageFont.truetype(FredokaOne, 16)
-	msg = "min:"+"{0:.1f}".format(lowest_price_next_24h) + "p"
-	draw.text((right_column,69), msg, inky_display.BLACK, font)
-	# we know how many half hours to min price, now figure it out in hours.
-	minterval = (round(prices.index(lowest_price_next_24h)/2))
-	print ("minterval:"+str(minterval))
-	msg = "in:"+str(minterval)+"hrs"
-	draw.text((right_column,85), msg, inky_display.BLACK, font)
-
-	# and convert that to an actual time
-	# note that this next time will not give you an exact half hour if you don't run this at an exact half hour eg cron
-	# because it's literally just adding n * 30 mins!
-	# could in future add some code to round to 30 mins increments but it works for now.
-
-	min_offset = prices.index(lowest_price_next_24h) * 30
-	time_of_cheapest = the_now_local + datetime.timedelta(minutes=min_offset)
-	print("cheapest at " + str(time_of_cheapest))
-	print("which is: "+ str(time_of_cheapest.time())[0:5])
-	time_of_cheapest_formatted = "at " + (str(time_of_cheapest.time())[0:5])
-	font = ImageFont.truetype(FredokaOne, 16)
-	draw.text((right_column,101), time_of_cheapest_formatted, inky_display.BLACK, font)
+	draw_all(
+		current_font_size=72,
+		current_y_offset=-10,
+		right_column_offset=172,
+		right_column_text_size=23,
+		bar_width=3.5,
+		bar_height_per_p=2.3,
+		bar_base_offset=121,
+		lowest_price_font_size=16,
+		lowest_price_y_offset=69
+		)
 
 
 # render the actual image onto the display
